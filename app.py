935c215c717459fa5b2356d9b5ad43e6b761c0f1
Thalya da Silva Batista <thalya.batista@infraestrutura.ac.gov.br>
2022-04-13 16:06:22 -0500

    Rest API com Flask

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request, json
+
+app = Flask(__name__)
+
+@app.route('/<int:id>')
+def pessoa(id):
+    soma = 1 + id
+    return jsonify({'id': id, 'nome':'Thalya', 'profissao':'Desenvolvedora'})
+
+#@app.route('/soma/<int:valor1>/<int:valor2>/')
+#def soma(valor1, valor2):
+#    return jsonify({'soma': valor1 + valor2})
+
+@app.route('/soma', methods=['POST','GET'])
+def soma():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        total = sum(dados['valores'])
+    elif request.method == 'GET':
+        total = 10 + 10
+    return jsonify({'soma': total})
+
+if __name__ == '__main__':
+    app.run(debug=True)
